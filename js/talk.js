94fa35f8a81e070215b1de78aa45177bf06221a9
2502797718 <2502797718@qq.com>
2024-05-21 22:42:27 +0800

    2th modify
    2th modify

--- js/talk.js
+++ js/talk.js
@@ -3,7 +3,7 @@ new TypeIt("#tqyxhj", {
         cursorSpeed: 1000,
         speed: 100
     })
-    .type("小洁洁 && 小怡怡")
+    .type("奈良 && 霞霞")
     .pause(2000)
     .delete(null, {
         delay: 500
